5cc517d25912ad90e45a17fd418a22f1d2f25ed1
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-08-27 19:26:14 +0900

    adding exp plot

--- src/IO_files_creator/auto_graph.py
+++ src/IO_files_creator/auto_graph.py
@@ -2,10 +2,6 @@ from IO_files_creator import graphs
 
 
 def plot_exp_by_gas(path_to_exp_sheet: str, heatr_list: [], colour_list: [], gas_list: [], unit: str):
-    # create mechanisms from input folders:
-    # okafor = graphs.Solution(
-    #     '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/chemkin_plots/okafor-solution')
-    # gri = graphs.Solution('/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/chemkin_plots/gri')
 
     for g in gas_list:
         if unit == 'ppm':
@@ -30,24 +26,37 @@ def plot_exp_by_gas(path_to_exp_sheet: str, heatr_list: [], colour_list: [], gas
         # scatter=True, best_fit_line=True, colour = 'g')
         graphy.show_and_save('chemkin_launch_files/graphs/', f'test_{g}.png')
 
-# gasl = ['H2', 'O2']
-# for g in gasl:
-#     graphy = graphs.Graph('equivalence ratio', g + ' concentration, %',
-#                           g + ' Concentration for all Heat Ratios')
-#     for h in heatr:
-#         graphy.add_scatter_spreadsheet('chemkin_launch_files/input_files/hcn_super_filtered.xlsx', 'mean_eqy',
-#                                        y='X_' + g, legend=f'experiment, {h} heat ratio', colour='r', error_colour='darkgrey', y_error='delta_X_' + g,
-#         filter_condition={'mean_heaty': float(h)}, best_fit_line=True,
-#         best_fit_line_filter={'mean_heaty': float(h)})
-#
-#         graphy.add_scatter_sol(okafor,'name1', g, legend=f'experiment, {h} heat ratio', filter_condition={'name2': h}, X_value='2.0000', number_of_points=1, multip=1000000,
-#         scatter=True, best_fit_line=True, colour='b')
-#         graphy.add_scatter_sol(gri, 'name1', g, legend=f"GRI-Mech3.0, {h} heat ratio", filter_condition={'name2': h}, X_value='2.0000', number_of_points=1, multip=1000000,
-#         scatter=True, best_fit_line=True, colour = 'g')
-#     graphy.show_and_save('chemkin_launch_files/graphs/', f'{g}all.png')
 
+def plot_all_by_gas_by_heat(path_to_exp_sheet: str, heatr_list: [], colour_list: [], gas_list: [], unit: str):
+    # create mechanisms from input folders:
+    okafor = graphs.Solution(
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/chemkin_plots/okafor-solution')
+    gri = graphs.Solution('/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/chemkin_plots/gri')
 
-# graphy = graphs.Graph('equivalence ratio', 'temperature, K', 'Actual Wall Temperature vs Equivalence Ratio')
+    for g in gas_list:
+        if unit == 'ppm':
+            graphy = graphs.GraphSetAxis('equivalence ratio', g + ' concentration, ppmv',
+                                         g + ' Concentration for all Heat Ratios')
+        else:
+            graphy = graphs.GraphSetAxis('equivalence ratio', g + ' concentration, %',
+                                         g + ' Concentration for all Heat Ratios')
+
+        for h, c in zip(heatr_list, colour_list):
+            # create a new graph for every iteration of gas:
+            graphy.add_scatter_spreadsheet(path_to_exp_sheet, 'mean_eqy', y='X_' + g,
+                                           legend=f'experiment, {h} heat ratio',
+                                           colour=c, filter_condition={'mean_heaty': float(h)}, best_fit_line=True,
+                                           y_error='delta_X_' + g, error_colour='darkgrey',
+                                           best_fit_line_filter={'mean_heaty': float(h)})
+
+        # graphy.add_scatter_sol(okafor, 'name1', g, legend=f"Okafor et al., {h} heat ratio",
+        #                        filter_condition={'name2': h}, X_value='2.0000', number_of_points=1, multip=1000000,
+        #                        scatter=True, best_fit_line=True, colour= 'b')
+        # graphy.add_scatter_sol(gri, 'name1', g, legend=f"GRI-Mech3.0, {h} heat ratio", filter_condition={'name2': h}, X_value='2.0000', number_of_points=1, multip=1000000,
+        # scatter=True, best_fit_line=True, colour = 'g')
+        graphy.show_and_save('chemkin_launch_files/graphs/', f'test_{g}.png')
+
+# graphy def plot_exp_by_gas(path_to_exp_sheet: str, heatr_list: [], colour_list: [], gas_list: [], unit: str):= graphs.Graph('equivalence ratio', 'temperature, K', 'Actual Wall Temperature vs Equivalence Ratio')
 # for h,c in zip(heatr, colour):
 #     # graphy.add_scatter_spreadsheet('chemkin_launch_files/input_files/hcn_super_filtered.xlsx','mean_eqy', 'Tw[K]', legend=h +' heat ratio', colour=c,
 #     #     filter_condition={'mean_heaty': float(h)}, best_fit_line=True, best_fit_line_filter={'mean_heaty': float(h)})
--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ from IO_files_creator import input, auto_graph
 
 def main():
     # define heat ratio, gas and colours:
-    h = ['0.2', '0.3', '0.4', '0.6']
-    colour = ['r', 'g', 'orange', 'cyan']
+    h = ['0.1', '0.2', '0.3', '0.4', '0.6']
+    colour = ['b','r', 'g', 'orange', 'cyan']
     gas = ['CO2', 'CO', 'H2O', 'NO', 'N2O', 'NO2', 'NH3', 'HCN']
 
     # create graphs ppmv:
-    auto_graph.plot_exp_by_gas('/Users/marina/Developer/GitHub/chemkin-plot-premix/import_files/augall.xlsx', h, colour, gas, 'ppm')
+    auto_graph.plot_exp_by_gas('/Users/marina/Developer/GitHub/chemkin-plot-premix/import_files/repeatfinal.xlsx', h, colour, gas, 'ppm')
 
     # create graphs %:
     gas = ['O2', 'H2']
-    auto_graph.plot_exp_by_gas('/Users/marina/Developer/GitHub/chemkin-plot-premix/import_files/augall.xlsx', h, colour, gas, 'percent')
+    auto_graph.plot_exp_by_gas('/Users/marina/Developer/GitHub/chemkin-plot-premix/import_files/repeatfinal.xlsx', h, colour, gas, 'percent')
 
 if __name__ == "__main__":
     main()
