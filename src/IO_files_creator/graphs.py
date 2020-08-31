6284773b18efa1852c8d41045db8cb1fa5f4cd0a
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-08-31 12:28:16 +0900

    merge extra func correct function

--- src/IO_files_creator/graphs.py
+++ src/IO_files_creator/graphs.py
@@ -128,12 +128,14 @@ class PremixSolution():
         :param self:
         :return:
         """
-        new_file = pd.DataFrame()  # Create an empty dataframe, this will be your final dataframe
+        new_file = pd.DataFrame()
+        df = pd.DataFrame()  # Create an empty dataframe, this will be your final dataframe
         print('getting first')
         for key, sub_df in self.df_dict.items():
             if not sub_df.empty:
-                print(sub_df)
-                df = sub_df.iloc[rownum,:]
+                df= sub_df.iloc[rownum,:]
+                df.set_index()
+                df.transpose()
                 new_file = pd.concat([new_file, df])  # Add your sub_df one by one
                 print(key)
 
